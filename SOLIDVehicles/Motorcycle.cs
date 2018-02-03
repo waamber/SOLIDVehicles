8ce064d6b6e951cf838a0404f332113c9d11acd6
waamber <amberastuart@aol.com>
2018-02-03 12:42:22 -0600

    file setup

---
+++ SOLIDVehicles/Motorcycle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDVehicles
+{
+    class Motorcycle : IVehicle
+    {
+        public int Wheels { get; set; } = 2;
+        public int Doors { get; set; } = 0;
+        public int PassengerCapacity { get; set; }
+        public bool Winged { get; set; } = false;
+        public string TransmissionType { get; set; } = "Manual";
+        public double EngineVolume { get; set; } = 1.3;
+        public double MaxWaterSpeed { get; set; }
+        public double MaxLandSpeed { get; set; } = 160.4;
+        public double MaxAirSpeed { get; set; }
+
+        public void Drive()
+        {
+            Console.WriteLine("The motorcycle screams down the highway");
+        }
+
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
